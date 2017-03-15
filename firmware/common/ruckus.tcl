1bd02a98e30ed6fe35fa73873ea38cae61793222
Benjamin Reese <bareese@slac.stanford.edu>
2017-03-15 10:30:56 -0700

    Initial commit of basic firmware project structure.

---
+++ firmware/common/ruckus.tcl
@@ -0,0 +1,6 @@
+# Load RUCKUS environment and library
+source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
+
+# Load ruckus files
+# Place an entry here for each common module
+#loadRuckusTcl "$::DIR_PATH/module1"
